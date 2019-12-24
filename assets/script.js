d9027ad61bd56d9e952c667062789c3f60e64d5e
McCabe Northup <mccabenorthup@gmail.com>
2019-12-23 17:18:12 -0700

    local storage has started working

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {  
-   
-   // set  day, date to header appended to title
+$(document).ready(function () {
+
+    // set  day, date to header appended to title
     $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
     // a table with rows up that append under container
 
@@ -22,7 +22,7 @@ $(document).ready(function () {
             var rowItem3 = $("<button><i class='fas fa-save'></i></button>").attr("class", "saveBtn col-md");
             row.append(rowItem1, rowItem2, rowItem3);
             $(".container").append(row);
-           
+
             // convert military time to am/pm
             if (i > 12) {
                 rowItem1.text(`${i - 12}:00 P.M. `);
@@ -42,33 +42,48 @@ $(document).ready(function () {
         } else {
             $(this).find(".description").addClass('future');
         }
-      
-    });    
 
+    });
+    let timeObject = {};
+    if (localStorage.getItem('timeObject')) {
+        timeObject = JSON.parse(localStorage.getItem('timeObject'));
+    }else{
+      timeObject = {
+        '9': { time: "9", value: ""},
+        '10':{ time: "10", value: ""},
+        '11':{ time: "11", value: ""},
+        '12':{ time: "12", value: ""},
+        '13':{ time: "13", value: ""},
+        '14':{ time: "14", value: ""},
+        '15':{ time: "15", value: ""},
+        '16':{ time: "16", value: ""},
+        '17':{ time: "17", value: ""}
+      };
+    }
 
-
-
-    var time = {};
     // click of save button saves items to local storage
-    $(".saveBtn").on("click", function () {
-        // get nearby values
-        var value = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        // save in localStorage
-        localStorage.setItem(time, value);
-        localStorage.getItem(value);
-        if (time == $("tr").attr("value")) {
-            $("textarea").html(value)
-
-        }
-        console.log(value)
-        console.log(time)
+    $(".time-block").each(function () {
+        $(this).find(".description").val(timeObject[$(this).attr("data-time")].value);
     });
 
+    //save value to local storage on click
+    $(".saveBtn").on('click', function (event) {
+        //set timeObject time attribute
+        var timeValue = $(this).closest(".time-block").attr("data-time");
+        //set timeObject value attribute
+        var textValue = $(this).closest(".time-block").find(".description").val();
+        timeObject[timeValue].value = textValue;
+
+        //save user input in each object to local storage
+        localStorage.setItem('timeObject', JSON.stringify(timeObject));
+
+        //console.log(textValue)
+
+    });
+});
 
 
-    // with click of save button, save enter input into local storage. 
-})
+
 
 // color changing is not quite working
 // wont save to local storage yet
